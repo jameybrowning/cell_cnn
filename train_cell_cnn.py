d213a34dfab096f8ba1b57b51da426a14aa6f049
jameybrowning <46007581+jameybrowning@users.noreply.github.com>
2019-02-16 11:37:33 -0500

    changed filename of cell_cnn

--- train_cell_cnn.py
+++ train_cell_cnn.py
@@ -2,20 +2,22 @@
 """
 Created on Thu Jan 24 15:06:20 2019
 
-@author: dan
+@author: jbrowning
 """
 import numpy as np
-import create_cell_cnn
+from create_cell_cnn_vgg16 import build_cnn
 import importlib
 import matplotlib.pyplot as plt
 from keras import optimizers
+import plot_prediction
+import scipy.io as sio
 #Create cnn and display summary
-importlib.reload(create_cell_cnn)
-cell_net = create_cell_cnn.create_cell_cnn()
+
+cell_net = build_cnn()
 cell_net.summary()
 
-#load image and target data from .m MATLAB files
-import scipy.io as sio
+#load image and target data from .mat MATLAB files
+
 images = sio.loadmat(r'C:\Users\VAMS_2\Dropbox\ML\cell_cnn\training_data\coins\trainImages')
 images = images['trainImages']
 images = np.transpose(images,(3,0,1,2))
@@ -23,26 +25,35 @@ images = np.float32(images/255)
 targets = sio.loadmat(r'C:\Users\VAMS_2\Dropbox\ML\cell_cnn\training_data\coins\outVector')
 targets = np.float32(targets['outVector'])
 
+#split data into training and validation 
 train_images = images[0:1801,:,:,:]
 train_targets = targets[0:1801,:]
 
 test_images = images[1801:,:,:,:]
 test_targets = targets[1801:,:]
 
-cell_net.compile(optimizer=optimizers.SGD(lr=0.05, momentum=0.9, decay=0.0, nesterov=False),
+#Compile and train network
+epochs = 400
+learning_rate = 0.01
+decay_rate = learning_rate / epochs
+momentum = 0.9
+batch_size = 8
+
+cell_net.compile(optimizer=optimizers.SGD(lr=learning_rate, momentum=momentum, decay=decay_rate, nesterov=False),
+#cell_net.compile(optimizer=optimizers.RMSprop(lr=learning_rate, rho=0.9, epsilon=None, decay=decay_rate),
               loss='mse',
               metrics = ['mae'])
 
 history = cell_net.fit(train_images,train_targets,
-                         epochs = 1800,
-                         batch_size = 32,
+                         epochs = epochs,
+                         batch_size = batch_size,
                          validation_data = (test_images, test_targets))
 
 prediction = cell_net.predict(test_images)
 
-import plot_prediction
+#Plot an image with prediction
 importlib.reload(plot_prediction)
-image_num = 1;
+image_num = 3;
 plot_prediction.plot_prediction(test_images[image_num,:,:,1], prediction[image_num,:], 7, 0.5)
 
 #plot_prediction.plot_prediction(test_images[image_num,:,:,1], test_targets[image_num,:], 7, 0.5)
